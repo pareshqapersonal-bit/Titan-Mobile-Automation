dc00ab3aaa5f34f048a352452e90e70c76541930
pareshqapersonal <pareshqapersonal@gmail.com>
2026-06-09 07:59:28 +0530

    Added new path code

--- src/test/java/Utilities/ConfigReader.java
+++ src/test/java/Utilities/ConfigReader.java
@@ -2,19 +2,25 @@ package Utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class ConfigReader {
 	 Properties prop;
+	 String path = null;
 
 	    public ConfigReader() {
 
 	        try {
+	        	path = Paths.get(
+	        		    System.getProperty("user.dir"),
+	        		    "Resources",
+	        		    "Config.properties"
+	        		).toString();
 
 	            FileInputStream fis =
 	                new FileInputStream(
-	                    System.getProperty("user.dir")
-	                    + "\\Resources\\Config.properties");
+	                    path);
 
 	            prop = new Properties();
 	            prop.load(fis);
